4f841cfb01bc25f5660b0339442cfe8ebe3fb7d1
marzifu <rifqyzein@gmail.com>
2023-03-28 10:21:53 +0700

    fixing stuff

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(user.routers)
 app.include_router(to.routers)
