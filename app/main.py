44cb1231005271eada95cd48010c7771c172db85
marzifu <rifqyzein@gmail.com>
2023-06-13 17:33:27 +0700

    restart

--- app/main.py
+++ app/main.py
@@ -39,9 +39,4 @@ app.include_router(admin.routers)
 @app.get("/")
 def home(db: Session = Depends(get_db)):
     return 0
-
-@app.get("/sentry-debug")
-async def trigger_error():
-    division_by_zero = 1 / 0
-    return division_by_zero
        
