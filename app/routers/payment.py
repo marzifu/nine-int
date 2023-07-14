92f1eee0d3bd3f1c8209cc5bf7eb0ee0e89468ae
marzifu <rifqyzein@gmail.com>
2023-07-14 13:16:41 +0700

    Order id fixing on payment

--- app/routers/payment.py
+++ app/routers/payment.py
@@ -28,7 +28,7 @@ def oneItem(id: int, db: Session = Depends(get_db), current_user: int = Depends(
 @routers.post("/handling")
 def handling(handles: schemas.Handling, db: Session = Depends(get_db)):
     if handles.transaction_status == "settlement" or handles.transaction_status == "capture":
-        order_post = models.midtransHandling(**handles.dict())
+        order_post = models.midtransHandling(order_id=handles.order_id,**handles.dict())
         db.add(order_post)
         db.commit()
         db.refresh(order_post)
